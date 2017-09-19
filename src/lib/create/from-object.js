d9c6405c0eb79666ec237cac43d9d572b796a466
Daniel Scholtus <sdeonline@gmail.com>
2017-09-19 07:38:36 -0300

    Set .isValid() == false for empty object.
    Empty string is invalid, empty object should be to.

--- src/lib/create/from-object.js
+++ src/lib/create/from-object.js
@@ -1,12 +1,18 @@
 import { normalizeObjectUnits } from '../units/aliases';
 import { configFromArray } from './from-array';
 import map from '../utils/map';
+import getParsingFlags from './parsing-flags';
 
 export function configFromObject(config) {
     if (config._d) {
         return;
     }
 
+    // Empty object, same behaviour as empty string
+    if (!config._i) {
+        getParsingFlags(config).empty = true;
+    }
+    
     var i = normalizeObjectUnits(config._i);
     config._a = map([i.year, i.month, i.day || i.date, i.hour, i.minute, i.second, i.millisecond], function (obj) {
         return obj && parseInt(obj, 10);
